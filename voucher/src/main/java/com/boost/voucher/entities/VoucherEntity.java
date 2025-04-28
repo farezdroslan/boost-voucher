087af707d9ac88922939c448df766825511627a6
farezdroslan <farezdzuanbinroslan@gmail.com>
2025-04-28 08:57:43 +0800

    created voucher entity

---
+++ voucher/src/main/java/com/boost/voucher/entities/VoucherEntity.java
@@ -0,0 +1,100 @@
+package com.boost.voucher.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class VoucherEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String code; // Unique voucher code
+    private String recipientName;
+    private String email; // Recipient's email
+    private String specialOfferName; // Name of the special offer
+    private Integer discountPercentage; // Discount percentage
+    private LocalDate expirationDate; // Expiration date
+    private LocalDate usageDate; // Date when the voucher was used
+
+    // Constructors, Getters, Setters
+    public VoucherEntity() {}
+
+    public VoucherEntity(String code, String recipientName, String email, String specialOfferName, Integer discountPercentage, LocalDate expirationDate) {
+        this.code = code;
+        this.email = email;
+        this.specialOfferName = specialOfferName;
+        this.discountPercentage = discountPercentage;
+        this.expirationDate = expirationDate;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setRecipientName(String recipientName) {
+        this.recipientName = recipientName;
+    }
+
+    public String getRecipientName() {
+        return recipientName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSpecialOfferName() {
+        return specialOfferName;
+    }
+
+    public void setSpecialOfferName(String specialOfferName) {
+        this.specialOfferName = specialOfferName;
+    }
+
+    public Integer getDiscountPercentage() {
+        return discountPercentage;
+    }
+
+    public void setDiscountPercentage(Integer discountPercentage) {
+        this.discountPercentage = discountPercentage;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public LocalDate getUsageDate() {
+        return usageDate;
+    }
+
+    public void setUsageDate(LocalDate usageDate) {
+        this.usageDate = usageDate;
+    }
+}
